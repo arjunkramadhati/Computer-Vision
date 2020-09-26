9da197bdc7d8c6bfb6b7cc920d628c5d98439bfe
arjunkramadhati <arjunkramadhatigopi@gmail.com>
2020-09-26 18:45:34 -0400

    Commit

--- homework4/cornerdetector.py
+++ homework4/cornerdetector.py
@@ -17,6 +17,7 @@ import time
 from scipy import signal as sg
 import tqdm
 import copy
+import threading
 
 
 class FeatureOperator:
@@ -29,6 +30,8 @@ class FeatureOperator:
         self.imagesizes = []
         self.filters = {}
         self.cornerpointdict = {}
+        self.slidingwindowdict = {}
+        self.correspondence = {}
         self.kvalue = kvalue
         for i in range(len(self.image_addresses)):
             self.originalImages.append(cv.imread(self.image_addresses[i]))
@@ -96,6 +99,40 @@ class FeatureOperator:
 
     # def harris_correspondence(self,queueImages, tags):
 
+    def get_sliding_windows(self, windowsize, queueImage,tag, dict, dicttag):
+
+        points = self.cornerpointdict[tag].flatten()
+        for index in range(len(points)):
+            if index == len(points) - 2:
+                break
+            row = points[index]
+            column = points[index+2]
+            dict[(row, column)] = self.grayscaleImages[queueImage][row:row+windowsize, column:column+windowsize]
+        self.slidingwindowdict[dicttag] = dict
+
+    def calculate_correspondence(self, style, tags):
+        windowsone = copy.deepcopy(self.slidingwindowdict[tags[0]])
+        windowstwo = copy.deepcopy(self.slidingwindowdict[tags[1]])
+        list = []
+        list2 = []
+        if style =="SSD":
+            for id1 in windowsone:
+                for id2 in windowstwo:
+                    ssd = np.sum(np.square(windowsone[id1]-windowstwo[id2]))
+                    windowstwo[id2]=ssd
+                    list.append(ssd)
+                    list2.append(id2)
+                ssd = min(ssd)
+                id = list2[list.index(ssd)]
+                windowsone[id1] = id
+            self.correspondence = windowsone
+
+
+
+    def print(self):
+        print(self.correspondence)
+
+
     def sift_corner_detect(self, queueImage, tag):
         keypoint, descriptor = self.siftobject.detectAndCompute(self.grayscaleImages[queueImage], None)
         self.cornerpointdict[tag] = (keypoint, descriptor)
@@ -115,13 +152,22 @@ class FeatureOperator:
 
 if __name__ == "__main__":
     tester = FeatureOperator(['hw4_Task1_Images/pair3/1.jpg','hw4_Task1_Images/pair3/2.jpg'], 1.407)
-    # tester.build_haar_filter()
-    # tester.determine_corners(1, 0, "Harris1")
-    # tester.determine_corners(1, 1, "Harris2")
-    # image = tester.draw_corner_points(0,"Harris1")
-    # cv.imwrite("1.jpg", image)
-    # image = tester.draw_corner_points(1, "Harris2")
-    # cv.imwrite("2.jpg", image)
-    tester.sift_corner_detect(0, "Sift1")
-    tester.sift_corner_detect(1, "Sift2")
-    tester.sift_correpondence((0, 1), ("Sift1","Sift2"))
+    tester.build_haar_filter()
+    tester.determine_corners(1, 0, "Harris1")
+    tester.determine_corners(1, 1, "Harris2")
+    image = tester.draw_corner_points(0,"Harris1")
+    cv.imwrite("1.jpg", image)
+    image = tester.draw_corner_points(1, "Harris2")
+    cv.imwrite("2.jpg", image)
+    thread_image_one = threading.Thread(target=tester.get_sliding_windows, args=(10,0,"Harris1",dict(),"Image1HarrisSW",))
+    thread_image_two = threading.Thread(target=tester.get_sliding_windows, args=(10, 1, "Harris2", dict(), "Image2HarrisSW", ))
+    thread_image_one.start()
+    thread_image_two.start()
+    thread_image_one.join()
+    thread_image_two.join()
+    tester.calculate_correspondence("SSD", ("Image1HarrisSW", "Image2HarrisSW"))
+    tester.print()
+
+    # tester.sift_corner_detect(0, "Sift1")
+    # tester.sift_corner_detect(1, "Sift2")
+    # tester.sift_correpondence((0, 1), ("Sift1","Sift2"))
