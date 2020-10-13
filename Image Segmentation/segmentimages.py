3fb12d7f214320e46c67900fa876094f231b4260
arjunkramadhati <arjunkramadhatigopi@gmail.com>
2020-10-12 21:06:22 -0400

    Commit

--- Image Segmentation/segmentimages.py
+++ Image Segmentation/segmentimages.py
@@ -35,6 +35,12 @@ class ImageSegmentation:
                 cv.resize(cv.imread(self.image_addresses[i], cv.IMREAD_GRAYSCALE), (640, 480)))
 
     def split_channels(self, inputstyle = 'BGR', gaussianblur = True):
+        """
+        Splits the RGB image into the three channels based on the way the image is read.
+        :param inputstyle: BGR is image is read using cv.imread()
+        :param gaussianblur: Yes/No image smoothening
+        :return: save the individual channels in dictionary
+        """
         for queue in range(len(self.originalImages)):
             if inputstyle == 'BGR':
                 r,g,b= self.originalImages[queue][:,:,2], self.originalImages[queue][:,:,1], self.originalImages[queue][:,:,0]
@@ -48,12 +54,26 @@ class ImageSegmentation:
             self.rgbchannelsdict[queue]={'R': r, 'G': g, 'B': b}
 
     def filter_masks(self, image):
+        """
+        Filter the output of the OTSU function using a median blur function.
+        :param image: Input image
+        :return: returns the filtered image
+        """
         filteredimage = cv.medianBlur(image, 13)
         filteredimage = filteredimage > 240
         filteredimage = np.array(filteredimage*255, np.uint8)
         return filteredimage
 
     def otsu_algorithm(self, image, imagequeue, index, method, iterations):
+        """
+        Iterative Otsu algorithm implementation.
+        :param image: Input image channel
+        :param imagequeue: Index value of the image position in the list
+        :param index: -
+        :param method: RGB/Texture based implementation
+        :param iterations: Number of iterations to run OTSU
+        :return: returns the OTSU threshold value
+        """
         otsucutoff = 0
         otsucutoffinitial = 0
         templist = []
@@ -94,6 +114,15 @@ class ImageSegmentation:
         return otsucutoff
 
     def run_otsu_texture(self, imagequeue, iterations, windows):
+        """
+        Texture based image segmenting implementation. We create three channels based
+        on the three window sizes. Each channel consists of the result from convoluting the
+        respective window.
+        :param imagequeue: Index value of the image position in the list
+        :param iterations: Number of iterations to run OTSU
+        :param windows: window sizes to extract texture
+        :return: draw and save the images with the contours and the foreground representations
+        """
         templist = []
         greyimage = self.grayscaleImages[imagequeue]
 
@@ -125,6 +154,16 @@ class ImageSegmentation:
         self.draw_contour_save(self.extract_contour(resultimage), imagequeue, 'texturemethod')
 
     def run_otsu_rgb(self, imagequeue, iterations):
+        """
+        RGB based image segmenting implementation. Using the three split channels, we run
+        the OTSU alorithm on each of the channels. We then filter the images based on the
+        OTSU cutoff. We combine the three images which will then be used by the contour
+        extractor.
+        :param imagequeue: Index value of the image position in the list
+        :param iterations: Number of iterations to run OTSU
+        :return: draw and save the images with the contours and the foreground representations
+
+        """
         templist = []
         for index, channel in enumerate(['R','G','B']):
             image = self.rgbchannelsdict[imagequeue][channel]
@@ -142,6 +181,13 @@ class ImageSegmentation:
         self.draw_contour_save(self.extract_contour(resultimage), imagequeue, 'rgbmethod')
 
     def draw_foreground_save(self, image, imagequeue, method):
+        """
+        Draw the foreground as black using the contour extracted.
+        :param image: Input image
+        :param imagequeue: Index value of the image position in the list
+        :param method: RGB/Texture based implementation
+        :return: Save the image
+        """
         r, g, b = self.rgbchannelsdict[imagequeue]['R'], self.rgbchannelsdict[imagequeue]['G'], self.rgbchannelsdict[imagequeue]['B']
         r,g,b = copy.deepcopy(r),copy.deepcopy(g),copy.deepcopy(b)
         truthplot = np.logical_and(np.logical_not(image),1)
@@ -152,6 +198,13 @@ class ImageSegmentation:
         cv.imwrite(str(imagequeue) +method+ 'foreground.jpg', resultimage)
 
     def draw_contour_save(self, contours, imagequeue, method):
+        """
+        Draw the foreground as black using the contour extracted.
+        :param contours: Contours we extracted using the extraction algorithm.
+        :param imagequeue: Index value of the image position in the list
+        :param method: RGB/Texture based implementation
+        :return: Save the image
+        """
         r,g,b = self.rgbchannelsdict[imagequeue]['R'],self.rgbchannelsdict[imagequeue]['G'],self.rgbchannelsdict[imagequeue]['B']
         r, g, b = copy.deepcopy(r), copy.deepcopy(g), copy.deepcopy(b)
         truthplot = np.logical_and(contours,1)
@@ -162,6 +215,13 @@ class ImageSegmentation:
         cv.imwrite(str(imagequeue)+method+'contourplot.jpg', resultimage)
 
     def extract_contour(self, image, style = 1):
+        """
+        Function to extract the contours from the post-OTSU comined image. For all
+        purposes we use the 8-neighbors window size to find the border pixels.
+        :param image: Input image of the combined channels
+        :param style: 1 for 8 neighbors, 2 for 4 neighbors
+        :return: Returns the array of the contours. 255 for border pixels. 0 for others.
+        """
         contourplot = np.zeros((image.shape[0],image.shape[1]))
         for row in range(1, image.shape[0]-1):
             for column in range(1, image.shape[1]-1):
@@ -178,11 +238,15 @@ class ImageSegmentation:
 
 
 if __name__ == '__main__':
+
+    """
+    Code starts here.
+    """
     tester = ImageSegmentation(['hw6_images/cat.jpg','hw6_images/pigeon.jpg','hw6_images/Red-Fox_.jpg'])
     tester.split_channels()
     iterations_rgb = [1,2,2]
     iterations_texture = [1,1,1]
-    windows=[19,21,22]
+    windows=[[5,7,9],[9,11,13],[19,21,23]]
     for i in range(len(iterations_rgb)):
         tester.run_otsu_rgb(i, iterations_rgb[i])
     for i in range(len(iterations_texture)):
