f11693c130f45e633f7e41010a2d30fde9669b66
arjunkramadhati <arjunkramadhatigopi@gmail.com>
2020-10-17 17:35:09 -0400

    Commit

---
+++ Image Classification/classifier.py
@@ -0,0 +1,16 @@
+"""
+Computer Vision - Purdue University - Homework 7
+
+Author : Arjun Kramadhati Gopi, MS-Computer & Information Technology, Purdue University.
+Date: Oct 19, 2020
+
+
+[TO RUN CODE]: python3 classifier.py
+Output:
+    [labels]: Predictions for the input images
+"""
+
+import cv2 as cv
+from matplotlib import pyplot as plt
+import numpy as np
+import copy
