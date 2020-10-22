4c61d909f705fa6ab6a10fc5173c718d8a435f78
arjunkramadhati <arjunkramadhatigopi@gmail.com>
2020-10-22 13:24:20 -0400

    commit

---
+++ Deep Learning Image Classification/main.py
@@ -0,0 +1,20 @@
+"""
+Computer Vision - Purdue University - Homework 8
+
+Author : Arjun Kramadhati Gopi, MS-Computer & Information Technology, Purdue University.
+Date: Oct 19, 2020
+
+
+[TO RUN CODE]: python3 deeplearnclassifier.py
+Output:
+    [labels]: Predictions for the input images
+"""
+
+import cv2 as cv
+from matplotlib import pyplot as plt
+import numpy as np
+import math
+import BitVector
+import pickle
+import os
+from collections import Counter
